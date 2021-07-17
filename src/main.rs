811f2e8ee4bba98aa6f81bb665ff2437457d73d0
yozhgoor <yozhgoor@outlook.com>
2021-07-17 08:41:50 +0200

    Add two const, respectively the help command and the help message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-}
+const HELP_COMMAND: &str = "!help";
+
+const HELP_MESSAGE: &str = "
+    Hello there, Human!
+
+    So you searching for help ?
+
+    No want will help you here, you can maybe ask on the <#CHANNEL_ID> if you dare.
+";
