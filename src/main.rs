23ccc07f6c9ec2b53df6bf1cd305d545be06dd42
yozhgoor <yozhgoor@outlook.com>
2021-07-17 08:45:20 +0200

    Create a struct Handler with two functions

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,20 @@ const HELP_MESSAGE: &str = "
 
     No want will help you here, you can maybe ask on the <#CHANNEL_ID> if you dare.
 ";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected", ready.user.name);
+    }
+}
